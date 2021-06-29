a07d784d988d91f1957a29ad71741755c67ee57f
Suzan Masite <suzanm@gerbertech.co.za>
2021-06-29 10:45:26 +0200

    Added Geotab Repository

---
+++ RoutePlanWebApi/RoutePlanWebApi/Repositories/Abstracts/IApiRepository.cs
@@ -0,0 +1,16 @@
+﻿using Geotab.Checkmate;
+using Geotab.Checkmate.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutePlanWebApi.Repositories.Abstracts
+{
+    public interface IApiRepository
+    {
+        bool Aunthenticate(API api);
+        User GetUserByUserName(string name);
+        
+    }
+}
---
+++ RoutePlanWebApi/RoutePlanWebApi/Repositories/Concrets/ApiRepository.cs
@@ -0,0 +1,39 @@
+﻿using Geotab.Checkmate;
+using Geotab.Checkmate.ObjectModel;
+using RoutePlanWebApi.Repositories.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutePlanWebApi.Repositories.Concrets
+{
+    public class ApiRepository : IApiRepository
+    {
+        private API _api;
+        public bool Aunthenticate(API api)
+        {
+            try
+            {
+                _api = api;
+                var user = GetUserByUserName(_api.UserName);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public User GetUserByUserName(string name)
+        {
+            return _api.CallAsync<List<User>>("Get", typeof(User), new
+            {
+                search = new UserSearch
+                {
+                    Name = name
+                }
+            }).Result.FirstOrDefault();
+        }
+    }
+}
