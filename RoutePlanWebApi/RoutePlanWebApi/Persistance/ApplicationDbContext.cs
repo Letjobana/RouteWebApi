9e67e34904131cf0317fd132fb4402f28d6d2a92
Suzan Masite <suzanm@gerbertech.co.za>
2021-06-29 09:41:16 +0200

    Added DbContext

---
+++ RoutePlanWebApi/RoutePlanWebApi/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoutePlannerWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutePlanWebApi.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<ApprovedRoutePlan> ApprovedRoutePlans { get; set; }
+        public DbSet<WeatherCondition> WeatherConditions { get; set; }
+        public DbSet<RoadCondition> RoadConditions { get; set; }
+
+        public DbSet<Trailer> Trailers { get; set; }
+
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        public DbSet<RoutePlanDetail> RoutePlanDetails { get; set; }
+        public DbSet<AdditionalHazard> AdditionalHazards { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    }
+}
